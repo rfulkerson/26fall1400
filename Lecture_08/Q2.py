76cd98b243110032bdadecff7b644b705f456b6c
Robert Fulkerson <rfulkerson@unomaha.edu>
2026-09-23 12:53:50 -0500

    Adding Lectures 8 and 9

---
+++ Lecture_08/Q2.py
@@ -0,0 +1,11 @@
+zeta = float(input())
+
+print("A", end="")
+
+if (zeta > 14.25):
+    print("B", end="")
+else:
+    print("C", end="")
+
+print("D", end="")
+
---
+++ Lecture_09/Q4.py
@@ -0,0 +1,11 @@
+# Which of the following expressions is equivalent to
+# not((x > 5) and (x < 10))
+
+x = int(input())
+print(not((x > 5) and (x < 10)))
+
+# print((x < 5) and (x > 10))
+# print((x <= 5) and (x >= 10))
+# print((x < 5) or (x > 10))
+# print((x <= 5) or (x >= 10))
+# print(not (x <= 5) and not (x >= 10))
